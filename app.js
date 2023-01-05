86bdc54d0736f07ce87d499da403b353e97cbb48
Elena <elena@kroupkin.com>
2023-01-05 13:14:47 +1300

    First commit for GitHub

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-
+const encrypt = require('mongoose-encryption');
 const app = express();
 
 app.use(express.static('public'));
@@ -16,10 +16,14 @@ mongoose.connect('mongodb://localhost:27017/userDB', {
   useNewUrlParser: true
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret,encryptedFields: ['password'] });
+
 
 const User = new mongoose.model("User", userSchema);
 
